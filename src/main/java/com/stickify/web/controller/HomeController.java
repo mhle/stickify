b83519cfddf90161bbb5dfeb45edeb0174eb4216
Joel Mata <joelmata2@gmail.com>
2012-06-01 00:26:29 +0200

    This is only a test
    This is only a test

--- src/main/java/com/stickify/web/controller/HomeController.java
+++ src/main/java/com/stickify/web/controller/HomeController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public class HomeController {
 
 	/**
-	 * Renders the home page as HTML in thw web browser.
+	 * Renders the home page as HTML in the web browser.
 	 * The home page is different based on whether the user is signed in or not.
 	 */
 	@RequestMapping(value="/", method=RequestMethod.GET)
