d6fceb7745b73c185d3862e080de5a30ae1bbf03
Michael Le <hle.michael@gmail.com>
2012-07-27 00:54:56 +0200

    added txmanagement annotation and spring aspectj

--- src/main/java/com/stickify/config/DatabaseConfig.java
+++ src/main/java/com/stickify/config/DatabaseConfig.java
@@ -6,16 +6,19 @@ import javax.inject.Inject;
 import javax.sql.DataSource;
 
 import org.hibernate.ejb.HibernatePersistence;
+import org.springframework.context.annotation.AdviceMode;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import com.jolbox.bonecp.BoneCPDataSource;
 
 @Configuration
+@EnableTransactionManagement(mode=AdviceMode.ASPECTJ)
 @PropertySource("classpath:application.properties")
 public class DatabaseConfig {
 	
